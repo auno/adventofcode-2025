6db266ea7907ef0a56d3fdbd2bfc92b655add87f
Mikael Auno <mikael@auno.se>
2025-12-01 11:19:37 +0100

    day01: Finished part 1

---
+++ src/day01.rs
@@ -0,0 +1,95 @@
+use anyhow::{bail, Error, Result};
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+
+enum Rotation {
+    Left(i32),
+    Right(i32),
+}
+
+impl TryFrom<&str> for Rotation {
+    type Error = Error;
+
+    fn try_from(value: &str) -> Result<Self> {
+        let Some((direction, distance)) = value.split_at_checked(1) else {
+            bail!("Invalid Rotation: {value}");
+        };
+
+        let Ok(distance) = distance.parse() else {
+            bail!("Invalid Rotation distance: {distance} (in {value})");
+        };
+
+        if distance < 0 {
+            bail!("Invalid Rotation distance: {distance} (in {value})");
+        }
+
+        match direction {
+            "L" => Ok(Rotation::Left(distance)),
+            "R" => Ok(Rotation::Right(distance)),
+            _ => bail!("Invalid Rotation direction: {direction} (in {value})"),
+        }
+    }
+}
+
+type Input = Vec<Rotation>;
+
+#[aoc_generator(day1)]
+fn parse(input: &str) -> Result<Input> {
+    input
+        .lines()
+        .map(|line| line.try_into())
+        .try_collect()
+}
+
+fn sequence(rotations: &[Rotation]) -> Vec<i32> {
+    rotations
+        .iter()
+        .fold(vec![50], |mut seq, rotation| {
+            let position = seq.last().unwrap();
+            let next_position = match rotation {
+                Rotation::Left(distance) => (position - distance).rem_euclid(100),
+                Rotation::Right(distance) => (position + distance).rem_euclid(100),
+            };
+
+            seq.push(next_position);
+            seq
+        })
+}
+
+#[aoc(day1, part1)]
+fn part1(rotations: &Input) -> usize {
+    sequence(rotations)
+        .into_iter()
+        .filter(|position| *position == 0)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use super::*;
+
+    const EXAMPLE1: &str = indoc! {"
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+    "};
+
+    #[test]
+    fn part1_example1() {
+        assert_eq!(3, part1(&parse(EXAMPLE1).unwrap()));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(1021, part1(&parse(include_str!("../input/2025/day1.txt")).unwrap()));
+    }
+}
