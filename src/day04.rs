73ab066b02086c995fef230232f6c6dab4b59731
Mikael Auno <mikael@auno.se>
2025-12-04 08:40:44 +0100

    day04: Finished part 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,7 +1,8 @@
 use anyhow::{bail, Result, Error};
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 
-use crate::utils::grid::Grid;
+use crate::utils::grid::{Grid, Position};
 
 #[derive(Copy, Clone, PartialEq, Eq)]
 enum Tile {
@@ -28,8 +29,7 @@ fn parse(input: &str) -> Result<Input> {
     Grid::parse(input)
 }
 
-#[aoc(day4, part1)]
-fn part1(grid: &Input) -> usize {
+fn list_removable_rolls(grid: &Input) -> impl IntoIterator<Item = Position> {
     grid.into_iter()
         .filter(|(_, tile)| **tile == Tile::RollOfPaper)
         .filter(|(position, _)| {
@@ -40,7 +40,36 @@ fn part1(grid: &Input) -> usize {
 
             count_neighboring_rolls < 4
         })
+        .map(|(p, _)| p)
+}
+
+#[aoc(day4, part1)]
+fn part1(grid: &Input) -> usize {
+    list_removable_rolls(grid)
+        .into_iter()
         .count()
+}
+
+#[aoc(day4, part2)]
+fn part2(grid: &Input) -> usize {
+    let mut grid = grid.clone();
+    let mut count = 0;
+
+    loop {
+        let removable = list_removable_rolls(&grid).into_iter().collect_vec();
+
+        if removable.is_empty() {
+            break;
+        }
+
+        count += removable.len();
+
+        for p in removable {
+            grid.set(&p, Tile::Floor);
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -70,5 +99,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(1578, part1(&parse(include_str!("../input/2025/day4.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(43, part2(&parse(EXAMPLE1).unwrap()));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(10132, part2(&parse(include_str!("../input/2025/day4.txt")).unwrap()));
     }
 }
