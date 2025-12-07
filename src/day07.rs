8a65249b4c8b91943d095b194b02fd8aaec2cc72
Mikael Auno <mikael@auno.se>
2025-12-07 11:33:01 +0100

    day07: Finished part 2

--- src/day07.rs
+++ src/day07.rs
@@ -2,7 +2,7 @@ use anyhow::{bail, Context, Error, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
-use crate::utils::grid::{Grid, Position};
+use crate::utils::grid::{Direction, Grid, Position};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Tile {
@@ -61,6 +61,44 @@ fn part1((grid, source_position): &Input) -> usize {
     count_splits
 }
 
+fn count_timelines(position: Position, grid: &Grid<Tile>) -> usize {
+    type Cache = Vec<Vec<Option<usize>>>;
+
+    fn count_timelines_cached(cache: &mut Cache, position: Position, grid: &Grid<Tile>) -> usize {
+        if let Some(count) = cache
+            .get(position.0 as usize)
+            .and_then(|rc| rc.get(position.1 as usize))
+            .copied()
+            .flatten()
+        {
+            return count;
+        }
+
+        let Some(tile) = grid.get(&position) else {
+            return 1;
+        };
+
+        let count = match tile {
+            Tile::Space => count_timelines_cached(cache, position.step(Direction::Down), grid),
+            Tile::Source => count_timelines_cached(cache, position.step(Direction::Down), grid),
+            Tile::Splitter => {
+                count_timelines_cached(cache, position.step(Direction::Left), grid)
+                    + count_timelines_cached(cache, position.step(Direction::Right), grid)
+            }
+        };
+
+        cache[position.0 as usize][position.1 as usize] = Some(count);
+        count
+    }
+
+    count_timelines_cached(&mut vec![vec![None; grid.cols()]; grid.rows()], position, grid)
+}
+
+#[aoc(day7, part2)]
+fn part2((grid, source_position): &Input) -> usize {
+    count_timelines(*source_position, grid)
+}
+
 #[cfg(test)]
 mod tests {
     use indoc::indoc;
@@ -94,5 +132,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(1546, part1(&parse(include_str!("../input/2025/day7.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(40, part2(&parse(EXAMPLE1).unwrap()));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(13883459503480, part2(&parse(include_str!("../input/2025/day7.txt")).unwrap()));
     }
 }
