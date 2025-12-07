3be460ccb5c679b5b22b578ee4523450a875ad31
Mikael Auno <mikael@auno.se>
2025-12-07 12:51:06 +0100

    day03: Rewrite with simpler and faster solution

--- src/day03.rs
+++ src/day03.rs
@@ -21,45 +21,34 @@ fn parse(input: &str) -> Result<Input> {
         .try_collect()
 }
 
-fn largest_joltage(bank: &[Joltage], num_batteries: usize) -> Option<Joltage> {
-    fn largest_joltage_with_cache(
-        cache: &mut Vec<Vec<Option<Option<usize>>>>,
-        bank: &[Joltage],
-        bank_offset: usize,
-        num_batteries: usize
-    ) -> Option<usize> {
-        if let Some(joltage) = cache[bank_offset][num_batteries] {
-            return joltage;
-        }
+fn largest_joltage(bank: &[Joltage], num_batteries: usize) -> Joltage {
+    let mut offset = 0;
+    let mut joltages = vec![];
 
-        let joltage =
-            if (bank.len() - bank_offset) < num_batteries {
-                None
-            } else if num_batteries == 1 {
-                bank[bank_offset..].iter().max().copied()
-            } else {
-                (bank_offset..bank.len())
-                    .filter_map(|battery| {
-                        let battery_joltage = bank[battery];
-                        let subsequent_joltage = largest_joltage_with_cache(cache, bank, battery + 1, num_batteries - 1)?;
-                        Some(battery_joltage * 10usize.pow((num_batteries - 1) as u32) + subsequent_joltage)
-                    })
-                    .max()
-            };
+    for batteries_left in (1..=num_batteries).rev() {
+        let (new_offset, joltage) = bank
+            .iter()
+            .enumerate()
+            .take(bank.len() - (batteries_left - 1))
+            .skip(offset)
+            .rev()
+            .max_by_key(|(_, joltage)| **joltage)
+            .unwrap();
 
-        cache[bank_offset][num_batteries] = Some(joltage);
-        joltage
+        joltages.push(*joltage);
+        offset = new_offset + 1;
     }
 
-    let mut cache = vec![vec![None; 13]; bank.len() + 1];
-    largest_joltage_with_cache(&mut cache, bank, 0, num_batteries)
+    joltages
+        .into_iter()
+        .fold(0, |acc, v| acc * 10 + v)
 }
 
 #[aoc(day3, part1)]
 fn part1(input: &Input) -> Joltage {
     input
         .iter()
-        .filter_map(|bank| largest_joltage(bank, 2))
+        .map(|bank| largest_joltage(bank, 2))
         .sum()
 }
 
@@ -67,7 +56,7 @@ fn part1(input: &Input) -> Joltage {
 fn part2(input: &Input) -> Joltage {
     input
         .iter()
-        .filter_map(|bank| largest_joltage(bank, 12))
+        .map(|bank| largest_joltage(bank, 12))
         .sum()
 }
 
@@ -86,42 +75,42 @@ mod tests {
 
     #[test]
     fn largest_joltage_example1_bank1_2() {
-        assert_eq!(Some(98), largest_joltage(&parse_bank("987654321111111").unwrap(), 2));
+        assert_eq!(98, largest_joltage(&parse_bank("987654321111111").unwrap(), 2));
     }
 
     #[test]
     fn largest_joltage_example1_bank2_2() {
-        assert_eq!(Some(89), largest_joltage(&parse_bank("811111111111119").unwrap(), 2));
+        assert_eq!(89, largest_joltage(&parse_bank("811111111111119").unwrap(), 2));
     }
 
     #[test]
     fn largest_joltage_example1_bank3_2() {
-        assert_eq!(Some(78), largest_joltage(&parse_bank("234234234234278").unwrap(), 2));
+        assert_eq!(78, largest_joltage(&parse_bank("234234234234278").unwrap(), 2));
     }
 
     #[test]
     fn largest_joltage_example1_bank4_2() {
-        assert_eq!(Some(92), largest_joltage(&parse_bank("818181911112111").unwrap(), 2));
+        assert_eq!(92, largest_joltage(&parse_bank("818181911112111").unwrap(), 2));
     }
 
     #[test]
     fn largest_joltage_example1_bank1_12() {
-        assert_eq!(Some(987654321111), largest_joltage(&parse_bank("987654321111111").unwrap(), 12));
+        assert_eq!(987654321111, largest_joltage(&parse_bank("987654321111111").unwrap(), 12));
     }
 
     #[test]
     fn largest_joltage_example1_bank2_12() {
-        assert_eq!(Some(811111111119), largest_joltage(&parse_bank("811111111111119").unwrap(), 12));
+        assert_eq!(811111111119, largest_joltage(&parse_bank("811111111111119").unwrap(), 12));
     }
 
     #[test]
     fn largest_joltage_example1_bank3_12() {
-        assert_eq!(Some(434234234278), largest_joltage(&parse_bank("234234234234278").unwrap(), 12));
+        assert_eq!(434234234278, largest_joltage(&parse_bank("234234234234278").unwrap(), 12));
     }
 
     #[test]
     fn largest_joltage_example1_bank4_12() {
-        assert_eq!(Some(888911112111), largest_joltage(&parse_bank("818181911112111").unwrap(), 12));
+        assert_eq!(888911112111, largest_joltage(&parse_bank("818181911112111").unwrap(), 12));
     }
 
     #[test]
